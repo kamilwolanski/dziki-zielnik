6879d40633525787558e5dd3690195ebb24ba06e
Kamil <kwolanski3@gmail.com>
2026-03-09 12:49:01 +0100

    chore(mobile): upgrade expo and dependant packages to 55

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Hello World!</Text>
     </View>
   )
 }
